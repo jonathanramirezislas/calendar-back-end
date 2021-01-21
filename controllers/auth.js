f808d53c62ff3db14d884233435c9219cc9f3130
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-01-20 18:46:37 -0600

    create user
    Using model, controller and bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,46 @@
 
 
 const {response} = require('express')
+const bcrypt = require('bcryptjs');
 
-const createUser= (req , res = response ) => {
+const User = require('../models/User');
 
-    const { name, email, password } = req.body; 
- 
-    res.json({
-        ok:true,
-        msg:'get',
 
-    })
+const createUser= async (req , res = response ) => {
+
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if ( User ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User is already registered'
+            });
+        }
+        //
+        user = new User( req.body );
+    
+        // encrypt password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync( password, salt );
+
+        //save user 
+        await user.save();
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Error from server'
+        });
+    }
 }
 
 
