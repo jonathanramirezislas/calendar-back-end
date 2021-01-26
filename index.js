6c3c42987725ae3ad766075d6535af4a5b40ccd9
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-01-25 20:48:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,17 @@ dbConnection();
 
 
 // CORS ..alllow any ip from outside to access our endpoints
-app.use(cors());
+//app.use(cors());
 
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //Directorio publico
 app.use(express.static('public'));
